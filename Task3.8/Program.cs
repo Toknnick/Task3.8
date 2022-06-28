60f0f8765fbcf9009fb3d13b0016fd9ce5464352
Toknnick <106018075+Toknnick@users.noreply.github.com>
2022-06-28 15:32:10 +0300

    Update Program.cs

--- Task3.8/Program.cs
+++ Task3.8/Program.cs
@@ -6,29 +6,41 @@ namespace Task3._8
     {
         static void Main(string[] args)
         {
+            bool isWork = true;
             int[] arrayOfNumbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             Console.WriteLine("Массив:");
 
-            for (int i = 0; i < arrayOfNumbers.Length; i++)
-            {
-                Console.Write(arrayOfNumbers[i] + " ");
-            }
+            foreach (var number in arrayOfNumbers)
+                Console.Write(number + " ");
 
-            Console.WriteLine("\nВведите на сколько позиций передвинуть массив:");
-            int position = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nЧтобы сдвинуть массив введите число 1:");
+            int number1 = 0;
             Console.WriteLine();
 
-            if (position > 0)
+            while (isWork)
             {
-                int[] temp = new int[position];
-                Array.Copy(arrayOfNumbers, temp, position);
-                Array.Copy(arrayOfNumbers, position, arrayOfNumbers, 0, arrayOfNumbers.Length - position);
-                Array.Copy(temp, 0, arrayOfNumbers, arrayOfNumbers.Length - position, temp.Length);
+                int position = int.Parse(Console.ReadLine());
+                int firstNumber = arrayOfNumbers[number1];
+
+                for (int i = 0; i < arrayOfNumbers.Length - position; i++)
+                {
+                    arrayOfNumbers[i] = arrayOfNumbers[i + position];
+                }
+
+                arrayOfNumbers[arrayOfNumbers.Length - position] = firstNumber;
 
                 foreach (var number in arrayOfNumbers)
                 {
                     Console.Write(number + " ");
                 }
+
+                if (position != 1)
+                {
+                    Console.WriteLine("Error\n");
+                    isWork = false;
+                }
+
+                Console.WriteLine();
             }
         }
     }

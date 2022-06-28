bad0bf1670b0a31ca48bfa8535980c10596bbeb2
Toknnick <106018075+Toknnick@users.noreply.github.com>
2022-06-28 14:30:07 +0300

    Update Program.cs

--- Task3.8/Program.cs
+++ Task3.8/Program.cs
@@ -6,27 +6,29 @@ namespace Task3._8
     {
         static void Main(string[] args)
         {
-            int[] arrayOfNumbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            int[] arrayOfNumbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             Console.WriteLine("Массив:");
-            int firstNumber = arrayOfNumbers[0];
 
-            foreach (var number in arrayOfNumbers)
-                Console.WriteLine(number);
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                Console.Write(arrayOfNumbers[i] + " ");
+            }
 
             Console.WriteLine("\nВведите на сколько позиций передвинуть массив:");
             int position = int.Parse(Console.ReadLine());
             Console.WriteLine();
 
-            for (int i = 0; i < arrayOfNumbers.Length - position; i++)
+            if (position > 0)
             {
-                arrayOfNumbers[i] = arrayOfNumbers[i + position];
-            }
+                int[] temp = new int[position];
+                Array.Copy(arrayOfNumbers, temp, position);
+                Array.Copy(arrayOfNumbers, position, arrayOfNumbers, 0, arrayOfNumbers.Length - position);
+                Array.Copy(temp, 0, arrayOfNumbers, arrayOfNumbers.Length - position, temp.Length);
 
-            arrayOfNumbers[arrayOfNumbers.Length - position] = firstNumber;
-
-            foreach (var number in arrayOfNumbers)
-            {
-                Console.WriteLine(number);
+                foreach (var number in arrayOfNumbers)
+                {
+                    Console.Write(number + " ");
+                }
             }
         }
     }
